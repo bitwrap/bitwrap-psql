d6f8793f89bc2687d413b32ef053f12a611bd062
stackdump <myork@stackdump.com>
2017-07-02 21:33:17 -0500

    WIP

--- entry.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-twistd -n bitwrap --listen-ip=0.0.0.0 --listen-port=8080 
--- setup.py
+++ setup.py
@@ -1,28 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="bitwrap-io",
+    name="bitwrap-psql",
     version="0.2.0",
     author="Matthew York",
     author_email="myork@stackdump.com",
-    description="A blockchain-style python eventstore w/ LMDB or postgresql backend",
+    description="a python api to for bitwrap-io to use PostgreSQL as an eventstore",
     license='MIT',
     keywords='PNML petri-net eventstore state-machine',
-    packages=find_packages() + ['twisted.plugins'],
+    packages=find_packages(),
     include_package_data=True,
-    install_requires=['cyclone==1.1', 'service-identity==16.0.0', 'pg8000==1.10.6'],
+    install_requires=['pg8000==1.10.6'],
     long_description="""
-# Bitwrap-io
+# Bitwrap-psql
 
-A blockchain-style eventstore
-
-### Reference
-
-Read Martin Fowler's description of [Event Sourcing](http://martinfowler.com/eaaDev/EventSourcing.html).
-
-Watch an event sourcing video from [Greg Young](https://www.youtube.com/watch?v=8JKjvY4etTY).
-
-Learn more about our deterministic approach to Blockchains at our blog [blahchain.com](http://www.blahchain.com).
+This library provides a database abstraction for the bitwrap-io package.
 
 """,
     url="http://getbitwrap.com",
